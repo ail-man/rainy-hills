1a0f3b31b4a419fa3f9b4b9f4aa741c08c4cdc0e
Artur Lomsadze <ailman1985@gmail.com>
2017-03-30 22:45:35 +0300

    Develop MBeans

--- src/main/java/com/ail/crxmarkets/mb/MbMain.java
+++ src/main/java/com/ail/crxmarkets/mb/MbMain.java
@@ -1,32 +1,26 @@
 package com.ail.crxmarkets.mb;
 
 import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-import com.ail.crxmarkets.ejb.RainyHillsEjbLocal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@ManagedBean(name = "mbRainyHills")
+@ManagedBean(name = "mbMain")
 @RequestScoped
-public class MbRainyHills {
-
-	public static final String PAGE_NAME = "rainyHills";
+public class MbMain {
 
 	private static final Logger log = LoggerFactory.getLogger(MbMain.class);
-
-	@EJB
-	private RainyHillsEjbLocal rainyHillsEjbLocal;
 
 	@PostConstruct
 	public void init() {
 		log.debug("Init MBean {} success", this.getClass().getName());
 	}
 
-	public String calculate() {
-		log.debug("{}.calculate() invoked", this.getClass().getName());
+	public String go() {
+		log.debug("{}.go() invoked", this.getClass().getName());
 		return MbRainyHills.PAGE_NAME;
 	}
+
 }
