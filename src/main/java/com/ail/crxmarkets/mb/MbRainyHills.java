16b4f21cba65ef0b2a849e95c4f962c62d8e9a41
Artur Lomsadze <ailman1985@gmail.com>
2017-04-01 11:12:12 +0300

    Draw Chart in web

--- src/main/java/com/ail/crxmarkets/mb/MbRainyHills.java
+++ src/main/java/com/ail/crxmarkets/mb/MbRainyHills.java
@@ -1,27 +1,36 @@
 package com.ail.crxmarkets.mb;
 
+import java.io.Serializable;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import com.ail.crxmarkets.ejb.RainyHillsEjbLocal;
+import com.ail.crxmarkets.logic.Surface;
+import com.ail.crxmarkets.logic.VesselAlgorithm;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @ManagedBean(name = "mbRainyHills")
 @ViewScoped
-public class MbRainyHills {
+public class MbRainyHills implements Serializable {
 
 	public static final String PAGE_NAME = "rainyHills";
+	private static final long serialVersionUID = 8572333733056959831L;
+	private static final String BAR_MODEL_TITLE = "Rainy Hills";
+	private static final String BAR_MODEL_LEGEND_POSITION = "ne";
+	private static final String BAR_MODEL_X_LABEL = "Point";
 
+	private static final String BAR_MODEL_Y_LABEL = "Height";
 	private static final Logger log = LoggerFactory.getLogger(MbMain.class);
 
-	private long totalWater;
-	private String calculationTime;
-
-	private int algorithm;
-	private String field;
+	private BarChartModel stackedVerticalModel;
 
 	@EJB
 	private RainyHillsEjbLocal rainyHillsEjbLocal;
@@ -29,27 +38,55 @@ public class MbRainyHills {
 	@PostConstruct
 	public void init() {
 		log.debug("Init MBean {} success", this.getClass().getName());
+		createBarModel();
+	}
+	//
+	//	public void calculate() {
+	//		log.debug("{}.calculate() invoked", this.getClass().getName());
+	//		rainyHillsEjbLocal.calcWaterOnSurface(new int[] { 1 }, 2);
+	//	}
+
+	private void createBarModel() {
+		stackedVerticalModel = new BarChartModel();
+
+		Surface surface = Surface.random(100, 0, 100);
+
+		ChartSeries surfaceChartSeries = new ChartSeries();
+		surfaceChartSeries.setLabel("Surface");
+
+		for (int i = 0; i < surface.getSurface().length; i++) {
+			surfaceChartSeries.set(i, surface.getSurface()[i]);
+		}
+
+		surface.fillWithWater(new VesselAlgorithm());
+
+		ChartSeries waterChartSeries = new ChartSeries();
+		waterChartSeries.setLabel("Water");
+		for (int i = 0; i < surface.getWater().length; i++) {
+			waterChartSeries.set(i, surface.getWater()[i]);
+		}
+
+		stackedVerticalModel.addSeries(surfaceChartSeries);
+		stackedVerticalModel.addSeries(waterChartSeries);
+
+		stackedVerticalModel.setStacked(true);
+
+		stackedVerticalModel.setBarMargin(1);
+		stackedVerticalModel.setBarPadding(1);
+
+		stackedVerticalModel.setTitle("Rainy Hills");
+		stackedVerticalModel.setLegendPosition("ne");
+
+		Axis xAxis = stackedVerticalModel.getAxis(AxisType.X);
+		xAxis.setLabel("Point");
+
+		Axis yAxis = stackedVerticalModel.getAxis(AxisType.Y);
+		yAxis.setLabel("Height");
+		yAxis.setMin(0);
+		yAxis.setMax(100);
 	}
 
-	public String calculate() {
-		log.debug("{}.calculate() invoked", this.getClass().getName());
-		rainyHillsEjbLocal.calcWaterOnSurface(new int[] { 1 }, 2);
-		return MbRainyHills.PAGE_NAME;
-	}
-
-	public long getTotalWater() {
-		return totalWater;
-	}
-
-	public String getCalculationTime() {
-		return calculationTime;
-	}
-
-	public int getAlgorithm() {
-		return algorithm;
-	}
-
-	public void setAlgorithm(int algorithm) {
-		this.algorithm = algorithm;
+	public BarChartModel getStackedVerticalModel() {
+		return stackedVerticalModel;
 	}
 }
