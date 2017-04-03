8d7e1cd820e661f05085eea7507264b61595fc9a
Artur Lomsadze <lomsadze@bpcbt.com>
2017-04-03 18:03:57 +0300

    Move all resource bundles to parent dir

--- src/main/java/com/ail/crxmarkets/jsf/ResourceBundles.java
+++ src/main/java/com/ail/crxmarkets/jsf/ResourceBundles.java
@@ -2,8 +2,8 @@ package com.ail.crxmarkets.jsf;
 
 public class ResourceBundles {
 	@SuppressWarnings("UnusedDeclaration")
-	public static final String MAIN = "com.ail.crxmarkets.locale.main.main";
-	public static final String RAINY_HILLS = "com.ail.crxmarkets.locale.rainyHills.rainyHills";
+	public static final String MAIN = "com.ail.crxmarkets.locale.main";
+	public static final String RAINY_HILLS = "com.ail.crxmarkets.locale.rainyHills";
 	@SuppressWarnings("WeakerAccess")
-	public static final String GENERAL = "com.ail.crxmarkets.locale.general.general";
+	public static final String GENERAL = "com.ail.crxmarkets.locale.general";
 }
